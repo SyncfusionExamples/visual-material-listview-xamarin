47044c70f574fe070d0da91a21ddc5b513ac91d8
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-28 12:00:46 +0530

    XAMARIN-34587 - Sample screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,74 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    #region Behavior
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+        SfListView ListView;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            ListView.ItemGenerator = new ItemGeneratorExt(this.ListView);
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+    }
+    #endregion
+
+    #region ItemGeneratorExt
+    public class ItemGeneratorExt : ItemGenerator
+    {
+        public ItemGeneratorExt(SfListView listview) : base(listview)
+        {
+
+        }
+        protected override ListViewItem OnCreateListViewItem(int itemIndex, ItemType type, object data = null)
+        {
+            if (type == ItemType.Record)
+                return new ListViewItemExt(type);
+            return base.OnCreateListViewItem(itemIndex, type, data);
+        }
+    }
+    #endregion
+
+    #region ListViewItemExt
+    public class ListViewItemExt : ListViewItem
+    {
+        public ListViewItemExt()
+        {
+        }
+        public ListViewItemExt(ItemType type) : base(type)
+        {
+
+        }
+        protected override void OnItemAppearing()
+        {
+            this.Opacity = 0;
+            this.FadeTo(1, 400, Easing.SinInOut);
+            base.OnItemAppearing();
+        }
+    }
+    #endregion
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Model/BookInfo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class BookInfo : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string bookName;
+        private string bookDesc;
+        private string bookAuthor;
+        #endregion
+
+        #region Constructor
+
+        public BookInfo()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string BookName
+        {
+            get { return bookName; }
+            set
+            {
+                bookName = value;
+                OnPropertyChanged("BookName");
+            }
+        }
+
+        public string BookDescription
+        {
+            get { return bookDesc; }
+            set
+            {
+                bookDesc = value;
+                OnPropertyChanged("BookDescription");
+            }
+        }
+
+        public string BookAuthor
+        {
+            get { return bookAuthor; }
+            set
+            {
+                bookAuthor = value;
+                OnPropertyChanged("BookAuthor");
+            }
+        }
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
